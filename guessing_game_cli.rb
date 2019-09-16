e1061b9c22533679c381ab843430a34df8fa12fd
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-09-16 01:13:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,18 @@
 def random_number
-  gets.chomp
+  rand(1...6)
 end
 
 def prompt_user
   "Guess a number between 1 and 6"
 end
 
-def 
+def guess_number
+  gets.chomp
+end
+
+def compare(stored_number, guess)
+  if stored_number == guess
+    puts "You guessed the correct number!"
+    elsif stored_number != guess
+      puts 
+  
